c949390684b427173a4684a1354464e1bdc35fe3
Nenad Pantelic <nenadpantelickg@gmail.com>
2019-05-12 20:38:10 +0200

    Nenad Pantelic | Add index.html

--- project/dist/js/pages/dashboard2.js
+++ project/dist/js/pages/dashboard2.js
@@ -17,7 +17,7 @@ $(function () {
   var salesChart       = new Chart(salesChartCanvas);
 
   var salesChartData = {
-    labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels  : ['Januar', 'Februar', 'Mart', 'April', 'Maj', 'Jun', 'Jul'],
     datasets: [
       {
         label               : 'Electronics',
