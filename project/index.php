6241f303bb53533b6cf02bad67d91f6bb57acfdb
Nikola Markovic <nikolaamaarkovic@gmail.com>
2019-05-24 22:24:02 +0200

    #1 | Nikola Markovic | Add profile picture and name for page index.php

--- project/index.php
+++ project/index.php
@@ -185,16 +185,16 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-              <span class="hidden-xs">Aca</span>
+              <?php echo '<img src="data:image;base64,'. $_SESSION["profile_picture"] .'" class="user-image" alt="User Image">'; ?>
+              <span class="hidden-xs"><?php echo $_SESSION["first_name"];?></span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <?php echo '<img src="data:image;base64,'. $_SESSION["profile_picture"] .'" class="img-circle" alt="User Image">'; ?>
 
                 <p>
-                  Aca - Web Developer
+                  <?php echo $_SESSION["first_name"] . " " . $_SESSION["last_name"] . " - " . $_SESSION["profession"]; ?>
                   <small>Član od Nov. 2012</small>
                 </p>
               </li>
@@ -222,10 +222,10 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+          <?php echo '<img src="data:image;base64,'. $_SESSION["profile_picture"] .'" class="img-circle" alt="User Image">'; ?>
         </div>
         <div class="pull-left info">
-          <p>Aca</p>
+          <p><?php echo $_SESSION["first_name"];?></p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
