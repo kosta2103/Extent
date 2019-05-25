131af52f9072ae9f5647fd42e6fdbc8e9e415ad1
Nikola Markovic <nikolaamaarkovic@gmail.com>
2019-05-25 12:33:53 +0200

    #1 | Nikola Markovic | Add automatically update profile picture of logged user

--- project/pages/backend_pages/edit_user_logic_logic.php
+++ project/pages/backend_pages/edit_user_logic_logic.php
@@ -1,4 +1,5 @@
 <?php 
+      session_start();
       require_once('../../database_connection.php');
       if(isset($_POST["register"])){
             try{
@@ -24,6 +25,9 @@
                   $profile_picture = addslashes($_FILES['profilepic']['tmp_name']);
                   $profile_picture = file_get_contents($profile_picture);
                   $profile_picture = base64_encode($profile_picture);
+                  if($id == $_SESSION["user_id"]){
+                        $_SESSION["profile_picture"] = $profile_picture;
+                  }
             	$sql_update = "UPDATE User SET first_name='" . $_POST['fname'] . "', last_name='" . $_POST['lname'] . "', username='". $_POST['username'] . "', password='" . $_POST['passwd'] . "', email='" . $_POST['mail'] . "', phone_number='" . $_POST['phonenum']. "', profession='" . $_POST['profession'] . "', profile_picture='" . $profile_picture . "', role_id='$role_id' WHERE user_id='$id'";
             	$stmt = $connection->prepare($sql_update);
             	$stmt->execute();
